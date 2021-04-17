c16300e476ae63f40f108c831c284aa2d46e16af
makayajb <makayajb@yahoo.fr>
2021-04-17 20:55:12 +0200

    github action

--- .github/scripts/synch.sh
+++ .github/scripts/synch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
 # Construct the exclude commands
-for excludedPath in $INPUT_EXCLUDE
+for excludedPath in $EXCLUDE
 do
   EXCLUDE="$EXCLUDE --exclude $excludedPath"
 done
 
-lftp $INPUT_FTP_HOST -u $INPUT_FTP_USERNAME,$INPUT_FTP_PASSWORD -e "$([ "$INPUT_DISABLE_SSL_CERTIFICATE_VERIFICATION" == true ] && echo "set ssl:verify-certificate false;") mirror --verbose --reverse $([ "$INPUT_DELETE" == true ] && echo "--delete") $([ "$INPUT_ONLY_NEWER" == true ] && echo "--only-newer") $([ "$INPUT_IGNORE_TIME" == true ] && echo "--ignore-time") $INPUT_OTHER_FLAGS $EXCLUDE $INPUT_LOCAL_SOURCE_DIR $INPUT_DIST_TARGET_DIR; exit"
+lftp $FTP_HOST -u $FTP_USERNAME,$FTP_PASSWORD -e "$([ "$DISABLE_SSL_CERTIFICATE_VERIFICATION" == true ] && echo "set ssl:verify-certificate false;") mirror --verbose --reverse $([ "$DELETE" == true ] && echo "--delete") $([ "$ONLY_NEWER" == true ] && echo "--only-newer") $([ "$IGNORE_TIME" == true ] && echo "--ignore-time") $OTHER_FLAGS $EXCLUDE $LOCAL_SOURCE_DIR $DIST_TARGET_DIR; exit"
